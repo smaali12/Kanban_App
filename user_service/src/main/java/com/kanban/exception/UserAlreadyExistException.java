4edeb690dbafc30b98378557d2500db943181ce2
Shradha Mali <112004302+smaali12@users.noreply.github.com>
2023-11-02 15:51:14 +0530

    Delete user_service directory

--- user_service/src/main/java/com/kanban/exception/UserAlreadyExistException.java
+++
@@ -1,9 +0,0 @@
-package com.kanban.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code= HttpStatus.CONFLICT,reason = "User already exist")
-public class UserAlreadyExistException extends  Exception{
-}
-
--- user_service/src/main/java/com/kanban/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kanban.repository;
-
-import com.kanban.domain.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User,String> {
-}
