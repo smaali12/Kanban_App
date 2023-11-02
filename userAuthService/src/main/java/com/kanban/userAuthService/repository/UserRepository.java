c77f1325f6bf9be6514853379df8492eb5ce8e39
Shradha Mali <112004302+smaali12@users.noreply.github.com>
2023-11-02 15:50:57 +0530

    Delete userAuthService directory

--- userAuthService/src/main/java/com/kanban/userAuthService/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kanban.userAuthService.repository;
-
-import com.kanban.userAuthService.domain.KanbanUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<KanbanUser,String> {
-}
--- userAuthService/src/main/java/com/kanban/userAuthService/service/TokenGenerator.java
+++
@@ -1,9 +0,0 @@
-package com.kanban.userAuthService.service;
-
-import java.util.Map;
-
-public interface TokenGenerator {
-    Map<String,String> getToken(String email);
-//    String getToken(String email);
-
-}
